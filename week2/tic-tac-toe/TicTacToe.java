4f50d19052b6321797a0414fe0ace4908f25d053
djudju12 <jonathan.willian321@gmail.com>
2023-05-15 16:51:38 -0300

    tictactoe

--- week2/tic-tac-toe/TicTacToe.java
+++ week2/tic-tac-toe/TicTacToe.java
@@ -15,8 +15,8 @@ public class TicTacToe {
         
         boolean continuarJogando = true;
         while(continuarJogando){
-            System.out.println("\nVez do " + proximoPlayer);
             printBoard(board);
+            System.out.println("Vez do " + proximoPlayer);
             System.out.println("Escolha a linha: ");
             posJogada[0] = s.nextInt();
             System.out.println("Escolha a coluna: ");
@@ -40,16 +40,18 @@ public class TicTacToe {
     }
 
     public static void printBoard(char[][] board) { 
+        System.out.println("Board:");
+        System.out.println(" 0   1   2\n");
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                System.out.print(board[i][j]);
+                System.out.print(" " + board[i][j]);
                 if (j < board.length-1) {
-                    System.out.print(" | ");
+                    System.out.print(" |");
                 }
             }
-            System.out.println();
+            System.out.println("   " + i);
             if (i < board.length-1) {
-                System.out.println("--|---|---");
+                System.out.println("---|---|---");
             }
             
         }
